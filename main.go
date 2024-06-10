c1320fbad8daa0bd31667f19d0a7865f424fd490
Abdulkadir <damadevkadir10@gmail.com>
2024-06-11 01:08:41 +0300

    init+fiber

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Helloasd")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Echo!")
+	})
+	e.Start(":8080")
 }
