7b28a008825cb53d4bda55ddd11320f662d7123c
Abdulkadir <damadevkadir10@gmail.com>
2024-06-11 23:30:23 +0300

    Connect db

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	"ToDo/handlers"
-	"github.com/labstack/echo/v4"
+	"ToDo/models"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/tasks", handlers.GetTasks)
-	e.Start(":8080")
+	//e := echo.New()
+	//e.GET("/tasks", handlers.GetTasks)
+	//e.Start(":8080")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	err = db.AutoMigrate(models.Task{})
+	if err != nil {
+		log.Fatal("Failed to migrate database")
+	}
+	newTask := models.Task{Id: 11, Title: "aaa", Description: "bbb", CreatedAt: time.Now(), UpdatedAt: time.Now()}
+	result := db.Create(&newTask)
+	if result.Error != nil {
+		log.Fatal("Faile to create new task")
+	}
+	var tasks []models.Task
+	db.Find(&tasks)
+	fmt.Println(tasks)
 }
--- models/data.go
+++ models/data.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Task struct {
+	gorm.Model
+	Id          int
+	Title       string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
