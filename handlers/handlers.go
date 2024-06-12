32461a35ecd9c8c2a0408d04e8f2c9fc079fca03
Abdulkadir <damadevkadir10@gmail.com>
2024-06-12 03:56:21 +0300

    GET width DB

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,43 @@
 package handlers
 
 import (
+	"ToDo/config"
+	"ToDo/models"
+	"fmt"
 	"github.com/labstack/echo/v4"
-	"net/http"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 func GetTasks(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, Echo!")
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	var tasks []models.Task
+	db.Find(&tasks)
+	fmt.Println(tasks)
+	return nil
+}
+
+func PostTasks(c echo.Context) error {
+	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	err = db.AutoMigrate(models.Task{})
+	if err != nil {
+		log.Fatal("Failed to migrate database")
+	}
+	newTask := models.Task{Id: 11, Title: "aaa", Description: "bbb", CreatedAt: time.Now(), UpdatedAt: time.Now()}
+	result := db.Create(&newTask)
+	if result.Error != nil {
+		log.Fatal("Failed to create new task")
+	}
+	var tasks []models.Task
+	db.Find(&tasks)
+	fmt.Println(tasks)
+	return nil
 }
--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"ToDo/models"
-	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"ToDo/handlers"
+	"github.com/labstack/echo/v4"
 	"log"
-	"time"
 )
 
 func main() {
-	//e := echo.New()
-	//e.GET("/tasks", handlers.GetTasks)
-	//e.Start(":8080")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	e := echo.New()
+	e.GET("/tasks", handlers.GetTasks)
+	err := e.Start(":8080")
 	if err != nil {
-		log.Fatal("Failed to connect to database")
+		log.Fatal("Error starting server:")
 	}
-	err = db.AutoMigrate(models.Task{})
-	if err != nil {
-		log.Fatal("Failed to migrate database")
-	}
-	newTask := models.Task{Id: 11, Title: "aaa", Description: "bbb", CreatedAt: time.Now(), UpdatedAt: time.Now()}
-	result := db.Create(&newTask)
-	if result.Error != nil {
-		log.Fatal("Faile to create new task")
-	}
-	var tasks []models.Task
-	db.Find(&tasks)
-	fmt.Println(tasks)
 }
