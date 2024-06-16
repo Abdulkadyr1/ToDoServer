b8d92c0ddc5aff3466245d7db83713a728000362
Abdulkadir <damadevkadir10@gmail.com>
2024-06-16 14:35:23 +0300

    Code-style

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"ToDo/internal/config"
-	"ToDo/pkg/tasks"
+	tasks2 "ToDo/tasks"
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -15,13 +15,13 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to connect to database")
 	}
-	err = db.AutoMigrate(tasks.Task{})
+	err = db.AutoMigrate(tasks2.Task{})
 	if err != nil {
 		log.Fatal("Failed to migrate database")
 	}
 	config.DB = db
 	e := echo.New()
-	TaskHandler := &tasks.TasksHandler{}
+	TaskHandler := &tasks2.TasksHandler{}
 	e.GET("/tasks", TaskHandler.GetAllTasks)
 	e.POST("/tasks", TaskHandler.PostTask)
 	e.PUT("/tasks/:id", TaskHandler.UpdateTask)
